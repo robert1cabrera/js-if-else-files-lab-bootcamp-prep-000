be92c657c7bf5e3bd23428747b35e540a29777c9
rob cabrera <robert1cabrera@gmail.com>
2019-01-20 22:53:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ if (name === "susan")
   
 } else {
   console.log("expected the name to be susan")
-}
+}if (height === "70")
+
+  console.log("the height is corrrect")
 
 // Don't worry about this
 module.exports = { name, height
